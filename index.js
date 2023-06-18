ec9d45264dc8178320ec661b4294d842fd6d12b9
Joshua Green <joshuamdgreen@outlook.com>
2023-06-18 12:42:41 -0400

    javascript update

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const image = document.querySelector('#PP');
 const body = document.body;
 
-image.addEventListener('mouseenter', 'click', () => {
+function addBlur() {
   body.classList.add('blurred');
-});
+}
 
-image.addEventListener('mouseleave', 'click', () => {
+function removeBlur() {
   body.classList.remove('blurred');
-});
+}
+
+image.addEventListener('mouseenter', addBlur);
+image.addEventListener('mouseleave', removeBlur);
+image.addEventListener('touchstart', addBlur);
+image.addEventListener('touchend', removeBlur);

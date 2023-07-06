02ed75a36b8754ea4aac0d986cd151204fe5a67d
Joshua Green <joshuamdgreen@outlook.com>
2023-07-06 06:28:01 -0400

    nav fix

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const image = document.querySelector('#PP');
 const body = document.body;
+// Retrieve the user agent string
+var userAgent = navigator.userAgent;
 
 function addBlur() {
   body.classList.add('blurred');
@@ -9,5 +11,17 @@ function removeBlur() {
   body.classList.remove('blurred');
 }
 
+
+// Check if the user agent string contains 'Instagram'
+if (userAgent.includes('Instagram')) {
+  // Get the <ul> element you want to remove
+  var ulElement = document.getElementById('*-image');
+
+  // Check if the <ul> element exists
+  if (ulElement) {
+    // Remove the <ul> element
+    ulElement.parentNode.removeChild(ulElement);
+  }
+}
 image.addEventListener('mouseenter', addBlur);
 image.addEventListener('mouseleave', removeBlur);

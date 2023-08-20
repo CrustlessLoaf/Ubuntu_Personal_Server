30fc19e104f3b573109943a65f540dc53e073bca
Joshua Green <joshuamdgreen@outlook.com>
2023-08-20 14:09:50 -0400

    weekend update

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,14 @@
   const image = document.querySelector('#PP');
   const body = document.body;
 
+  document.addEventListener("DOMContentLoaded", function(event) {
+    document.body.style.opacity = "1"; // Set the opacity to 1, making the body visible
+    document.body.style.transition="2.5s";
+  });
+
+  image.addEventListener('mouseenter', addBlur);
+  image.addEventListener('mouseleave', removeBlur);
+
   function addBlur() {
     body.classList.add('blurred');
   }
@@ -8,12 +16,4 @@
   function removeBlur() {
     body.classList.remove('blurred');
   }
-
-  image.addEventListener('mouseenter', addBlur);
-  image.addEventListener('mouseleave', removeBlur);
-
-  document.addEventListener("DOMContentLoaded", function(event) {
-    document.body.style.opacity = "1"; // Set the opacity to 1, making the body visible
-    document.body.style.transition="2.5s";
-  });
   
